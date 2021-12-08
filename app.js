c4538130197ecdebcf85540d22f33c8fdd1412bc
Chris Ear <christear85@gmail.com>
2021-12-08 07:29:11 -0700

    worked on javascript

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
+let squaresPerRow = 4
 const container = document.querySelector(".container")
+const clearBtn = document.querySelector(".clear-btn")
+clearBtn.addEventListener("click", clearGrid)
+let squares
+const slider = document.querySelector(".slider")
+const gridSize = document.querySelector(".grid-size")
 
-for(let i = 0; i < 16; i++){
+slider.oninput = function() {
+    gridSize.innerHTML = this.value;
+}
+
+function changeBackground(e){
+  let r = Math.floor(Math.random() * 255)
+  let g = Math.floor(Math.random() * 255)
+  let b = Math.floor(Math.random() * 255)
+  let color = `rgb(${r}, ${g}, ${b})`
+  e.target.style.background = color
+}
+
+function clearGrid(){
+  squares.forEach(square => square.style.background = "lightgrey")
+  squaresPerRow = prompt("How many squares per side?")
+  fillGrid()
+}
+
+function fillGrid(){
+  //clear grid first
+  container.innerHTML = ""
+  
+  //add columns and rows
+  container.style.gridTemplateRows = `repeat(${squaresPerRow}, 1fr)`
+  container.style.gridTemplateColumns = `repeat(${squaresPerRow}, 1fr)`
+  
+  let numSquares = squaresPerRow * squaresPerRow
+  
+  //create squares
+  for(let i = 0; i < numSquares; i++){
     let square = document.createElement("div")
     square.setAttribute("class", "square")
     container.appendChild(square)
+  }
+
+  //get array of squares
+  squares = document.querySelectorAll(".square")
+
+  //add listener for hover on each square
+  squares.forEach(square => square.addEventListener("mouseover", changeBackground))
 }
+
+fillGrid()
